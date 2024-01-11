3827f95cacabe4375545e3edf6cc86860f09a0f1
nihadcodes <nihad.exe@gmail.com>
2024-01-11 14:37:52 +0600

    leetcode day 2

---
+++ leetcode_14_longest_common_prefix.dart
@@ -0,0 +1,20 @@
+class Solution {
+    String longestCommonPrefix(List<String>strs) {
+        if(strs.isEmpty) {
+            return "";
+     
+        }
+        String prefix = strs[0];
+        for (int i =1; i<strs.length; i++) {
+            while(prefix.isNotEmpty && !strs[i].startsWith(prefix)) {
+                prefix =prefix.substring(0, prefix.length-1);
+            }
+            if (prefix.isEmpty) {
+                return "";
+            }
+            }
+
+            return prefix;
+
+        }
+    }
